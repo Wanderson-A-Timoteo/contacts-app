3c12c1a4c59b224c81e4ad0041f449cbc9de42d3
Wanderson-A-Timoteo <wanderson_timoteo@hotmail.com>
2020-07-25 19:23:24 -0400

    Criado file contacts.ts, biblioteca de contatos

---
+++ src/contacts.ts
@@ -0,0 +1,28 @@
+const CONTACTS = [
+  {
+      'id': '9aae3dd5-8bf6-489e-9a68-397884b150a1',
+      'name': 'Ted Wingham',
+      'email': 'ted@gmail.com',
+      'phoneNumber': '4532842852'
+  },
+  {
+      'id': '26bc3b5f-ff28-4282-890d-51467c2d5be2',
+      'name': 'Clark Greaser',
+      'email': 'clarck@gmail.com',
+      'phoneNumber': '5445824525'
+  },
+  {
+      'id': '1a15188c-5218-4ac0-80a0-366d84aa3ec5',
+      'name': 'Nuno Kubacki',
+      'email': 'nk@gmail.com',
+      'phoneNumber': '1549584561'
+  },
+  {
+      'id': '8206a388-81e8-4b9b-b8d1-17ca47354d76',
+      'name': 'Maia Schaumaker',
+      'email': 'maia@gmail.com',
+      'phoneNumber': '1355485466'
+  }
+];
+
+export default CONTACTS;
